5a25ee69afb446e5ea2d4d132e06e63ea0c1d52d
CLOMDY <118122654+CLOMDY@users.noreply.github.com>
2025-09-17 12:42:47 +0530

    python RPL done

---
+++ PYTHON/RPL.py
@@ -0,0 +1,11 @@
+student = ["Aadarsh", "Aditya", "Sneha", "Richa", "Mishu"]
+print(student)
+
+student.reverse()
+print(student)
+
+student[2] = "Anushka"
+print(student)
+
+student.pop()
+print(student)
