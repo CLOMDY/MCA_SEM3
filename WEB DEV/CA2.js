d38b07f75b9a0c5ee06b417cbff88ff38c7ddacc
CLOMDY <118122654+CLOMDY@users.noreply.github.com>
2025-11-04 12:23:34 +0530

    sem update

---
+++ WEB DEV/CA2.js
@@ -0,0 +1,16 @@
+// Goal: Write JavaScript code to filter and sort an array of objects based on specific conditions.
+// Steps:
+// 1.	Create an array of objects, where each object represents a student with the following properties:
+// o	name (string)
+// o	age (number)
+// o	score (number)
+// 2.	Filter the students who are older than 21.
+// 3 Sort the filtered students by score in descending order.
+// 4 Print the sorted and filtered array to the console.
+// Expected Outcome:
+// •	The students who are older than 21 should be displayed in the order of their scores, starting from the highest score.
+
+
+const students = [
+    {name: "Aadarsh", age: 24, score: 90},
+    
